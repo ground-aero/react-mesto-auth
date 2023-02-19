e71be18b38f5d6747db1c852cc1f49d1150901b1
Eugene Avdeev <ea.avdeev@gmail.com>
2023-02-19 18:04:57 +0300

    fix(popupwithform): button

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,20 +3,16 @@ import React from 'react';
 function PopupWithForm({ name, title, buttonText, isOpen, onClose, children, onSubmit }) {
     // console.log(children)
 
-    function onSubimBtn() {
-
-    }
-
     return (
 
         <div className={`popup popup_type_${ name } ${ isOpen ? 'popup_opened' : '' }`}>
             <div className="popup__container">
                 <button className="popup__btn-close opacity-transition" onClick={ onClose } type="button"
                         aria-label="close"></button>
-                <form onSubmit={onSubmit} action="src/components/App" className="popup__form" name={ name } >
+                <form onSubmit={onSubmit} className="popup__form" name={ name } >
                     <h2 className="popup__title">{ title }</h2>
                     { children }
-                    <button onClick={onSubimBtn} type="submit"
+                    <button type="submit"
                             className="btn btn_submit opacity-transition">{ buttonText }
                     </button>
                 </form>
